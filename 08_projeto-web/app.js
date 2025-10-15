86bcbaf21bdafb6f24178afdaf7d3dd7a50eee83
Matheus Ferreira Cardoso <matheus.cardoso@squadra.com.br>
2025-10-15 11:54:05 -0300

    CSS e HTML do Projeto Web

--- 08_projeto-web/app.js
+++ 08_projeto-web/app.js
@@ -1,12 +1,11 @@
-const express = require('express');
-
-const app = express();
-
-const db = require('./db/connection');
-
+const express    = require('express');
+const exphbs     = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT       = 3000;
 
 app.listen(PORT, function () {
     console.log(`O Express está rodando na porta ${PORT}`);
@@ -15,6 +14,9 @@ app.listen(PORT, function () {
 
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// handle bars
+app.set('views', path.join(__dirname, 'views'))
 
 // db connection
 db
